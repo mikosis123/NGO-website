4d2ebfb01deba27619fbb0c435355cd36b6ca2ba
mikotech sis <mikotechsis@gmail.com>
2025-09-28 09:39:36 +0000

    dont just remove the nav background make it translusent

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -28,7 +28,7 @@ export function Header() {
   return (
     <header className={cn(
         "fixed top-0 z-50 w-full transition-all duration-300",
-        scrolled ? "border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60" : "bg-transparent",
+        scrolled ? "border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60" : "bg-black/20",
         "mt-4"
     )}>
       <div className="container flex h-16 items-center">
