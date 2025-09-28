1206c960a42bab7872b45b9f515b7b0e6adb8fe7
mikotech sis <mikotechsis@gmail.com>
2025-09-28 10:07:24 +0000

    remove the white thing at the top of hero picture and at the bak of nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <div className="flex min-h-screen flex-col">
           <Header />
-          <main className="flex-1 pt-24">{children}</main>
+          <main className="flex-1">{children}</main>
           <Footer />
         </div>
         <Toaster />
