01f3502dd771503b03ad7c78a35d66c632a14096
mikotech sis <mikotechsis@gmail.com>
2025-09-30 18:27:48 +0000

    i want this data to be stored in .env for sequirity const firebaseConfig

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  "projectId": "studio-2825472322-56ef3",
-  "appId": "1:855558154389:web:39d71e08d8fff8a4e2939c",
-  "apiKey": "AIzaSyCtsk8cYuS5KjsWQnLQ-1HAQ2xveEkFAUw",
-  "authDomain": "studio-2825472322-56ef3.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "855558154389"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
