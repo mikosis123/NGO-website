6714fc8851380ee6f090eb80a7816d217f13d87e
mikotech sis <mikotechsis@gmail.com>
2025-10-01 08:29:02 +0000

    their was an eroor when i add and save the image says a"there was an err

--- src/app/(admin)/admin/gallery/gallery-form.tsx
+++ src/app/(admin)/admin/gallery/gallery-form.tsx
@@ -15,7 +15,8 @@ import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { useState } from 'react';
 
 const gallerySchema = z.object({
-  image: z.instanceof(FileList).refine((files) => files?.length === 1, 'Image is required.'),
+  image: z.any()
+    .refine((files) => files?.length === 1, 'Image is required.'),
   description: z.string().min(10, 'Description must be at least 10 characters.'),
   imageHint: z.string().min(2, 'Image hint must be at least 2 characters.'),
 });
