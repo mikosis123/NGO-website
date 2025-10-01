49aa1edf9a818dbe3120a8d561f770c9ab32a9a2
mikotech sis <mikotechsis@gmail.com>
2025-10-01 08:43:15 +0000

    its still not working

--- src/app/(admin)/admin/gallery/gallery-form.tsx
+++ src/app/(admin)/admin/gallery/gallery-form.tsx
@@ -15,7 +15,7 @@ import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { useState } from 'react';
 
 const gallerySchema = z.object({
-  image: z.any()
+  image: z.instanceof(FileList)
     .refine((files) => files?.length === 1, 'Image is required.'),
   description: z.string().min(10, 'Description must be at least 10 characters.'),
   imageHint: z.string().min(2, 'Image hint must be at least 2 characters.'),
