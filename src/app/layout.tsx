4201b6860e56659d52e2ac62ed8869fd336e3900
mikotech sis <mikotechsis@gmail.com>
2025-10-02 08:13:12 +0000

    Try fixing this error: `Runtime Error: Error: useFirebase must be used w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { AppLayoutClient } from '@/components/layout/app-layout-client';
+import { FirebaseAppProvider } from '@/firebase/provider';
 
 export const metadata: Metadata = {
   title: 'Light for Generation Ethiopia',
@@ -22,7 +23,9 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <AppLayoutClient>{children}</AppLayoutClient>
+        <FirebaseAppProvider>
+          <AppLayoutClient>{children}</AppLayoutClient>
+        </FirebaseAppProvider>
         <Toaster />
       </body>
     </html>
